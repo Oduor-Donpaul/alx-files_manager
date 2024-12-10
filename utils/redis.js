b1d7c47876434e6f0a8d728819f68b6ed69bbeb0
donpaul <donpaul.oduordonpaul@gmail.com>
2024-12-10 15:41:12 +0300

    file manager

---
+++ utils/redis.js
@@ -0,0 +1,57 @@
+// utils/redis.js
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    // Display any error of the redis client in the console
+    this.client.on('error', (err) => {
+      console.error('Redis client error:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+// Create and export an instance of RedisClient
+const redisClient = new RedisClient();
+module.exports = redisClient;
